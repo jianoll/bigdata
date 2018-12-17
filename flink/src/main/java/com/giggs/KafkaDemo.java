23fff26ad3c222469b8c3185375274548d731903
tom_giggs <zamjbn@163.com>
2018-12-17 21:50:02 +0800

    kafka demo

--- flink/src/main/java/com/giggs/KafkaDemo.java
+++ flink/src/main/java/com/giggs/KafkaDemo.java
@@ -1,35 +1,31 @@
 package com.giggs;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
 import org.apache.flink.util.Collector;
 
 import java.util.Properties;
 
-public class FileDemo {
-    //使用命令  .\flink.bat run  -jar D:\workspace\bigdata\flink\target\flink005.jar  --input file:///d:\dataset\wordcount.txt  --output file:///D:\progr
-    //am\flink-1.7.0\bin\out.txt 运行
+public class KafkaDemo {
     public static void main(String[] args) throws Exception {
         final ParameterTool params = ParameterTool.fromArgs(args);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(1000);
-        DataStream<String> stream=null;
-        if (!params.has("input")) {
-            stream = env.readTextFile(params.get("input"));
-        }else {
-            stream = env.readTextFile("file:///d:\\dataset\\wordcount.txt");
-        }
+
         Properties properties = new Properties();
-//        properties.setProperty("bootstrap.servers", "localhost:9092");
-//        properties.setProperty("group.id", "flink001");
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "flink001");
 //        String[] lines = {"this is a redis demo","i am a redis test"};
-
-//        DataStream<String> stream = env.fromElements(lines);
-
-
+        FlinkKafkaConsumer09<String> kafkaSource = new FlinkKafkaConsumer09<String>("flink_demo", new SimpleStringSchema(), properties);
+        DataStream<String> stream = env.addSource(kafkaSource);
+    //        DataStream<String> stream = env.fromElements(lines);
         DataStream<Tuple2<String, Integer>> counts = stream.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
 
             @Override
@@ -44,13 +40,16 @@ public class FileDemo {
         })
                 .keyBy(0)
                 .sum(1);
-        if(params.has("output"))
-        counts.writeAsCsv(params.get("output"));
-        else {
+        FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").setPort(6379).build();
+        //实例化RedisSink，并通过flink的addSink的方式将flink计算的结果插入到redis
+        counts.addSink(new RedisSink<Tuple2<String, Integer>>(conf, new App.RedisExampleMapper()));
+
+        if (params.has("output")) {
+            counts.writeAsText(params.get("output"));
+        } else {
+            System.out.println("Print result to stdout. Use --output to specify output path.");
             counts.print();
         }
-        //execute应该要在最后
-        env.execute("running read file demo");
-
+        env.execute("flink kafka demo");
     }
 }
