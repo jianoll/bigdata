e56a04b2dd3fb7c08f25b67552d2f98a863a9ea0
tom_giggs <zamjbn@163.com>
2018-12-19 11:21:30 +0800

    flink kafka demo

--- flink/src/main/java/com/giggs/KafkaDemo.java
+++ flink/src/main/java/com/giggs/KafkaDemo.java
@@ -4,8 +4,12 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 import org.apache.flink.streaming.connectors.redis.RedisSink;
 import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
@@ -14,18 +18,29 @@ import org.apache.flink.util.Collector;
 import java.util.Properties;
 
 public class KafkaDemo {
+    //  .\flink.bat run  -jar D:\workspace\bigdata\flink\target\flink008.jar
     public static void main(String[] args) throws Exception {
         final ParameterTool params = ParameterTool.fromArgs(args);
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.enableCheckpointing(1000);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.getConfig().setGlobalJobParameters(params);
+//        env.enableCheckpointing(1000);
 
         Properties properties = new Properties();
+        //一直没有在kafka消息里面看到连接请求的日志，不知道为什么，flink控制台也UI页面没有错误消息，一直在running状态,但是结果已经写入到redis中了
         properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("zookeeper.connect", "127.0.0.1:2181");
         properties.setProperty("group.id", "flink001");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("session.timeout.ms", "30000");
+//        new FlinkKafkaConsumer010();
 //        String[] lines = {"this is a redis demo","i am a redis test"};
-        FlinkKafkaConsumer09<String> kafkaSource = new FlinkKafkaConsumer09<String>("flink_demo", new SimpleStringSchema(), properties);
+//        FlinkKafkaConsumer09<String> kafkaSource = new FlinkKafkaConsumer09<String>("flink_demo", new SimpleStringSchema(), properties);
+        FlinkKafkaConsumer011<String> kafkaSource = new FlinkKafkaConsumer011<String>("flink_demo", new SimpleStringSchema(), properties);
+        kafkaSource.setStartFromEarliest();
         DataStream<String> stream = env.addSource(kafkaSource);
     //        DataStream<String> stream = env.fromElements(lines);
+
         DataStream<Tuple2<String, Integer>> counts = stream.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
 
             @Override
@@ -42,7 +57,7 @@ public class KafkaDemo {
                 .sum(1);
         FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").setPort(6379).build();
         //实例化RedisSink，并通过flink的addSink的方式将flink计算的结果插入到redis
-        counts.addSink(new RedisSink<Tuple2<String, Integer>>(conf, new App.RedisExampleMapper()));
+        counts.addSink(new RedisSink<>(conf, new App.RedisExampleMapper()));
 
         if (params.has("output")) {
             counts.writeAsText(params.get("output"));
@@ -50,6 +65,7 @@ public class KafkaDemo {
             System.out.println("Print result to stdout. Use --output to specify output path.");
             counts.print();
         }
+        ((SingleOutputStreamOperator<Tuple2<String, Integer>>) counts).setParallelism(1);
         env.execute("flink kafka demo");
     }
 }
